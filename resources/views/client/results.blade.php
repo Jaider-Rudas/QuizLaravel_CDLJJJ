2593de5a99aa736637962fded5d8d0d279d9e294
Jaider-Rudas <jmrudas6@misena.edu.co>
2022-08-24 14:34:28 -0500

    quiz con exportación excel, pdf, roles...

---
+++ resources/views/client/results.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>jeo</title>
+    <link rel="stylesheet" href="https://bootswatch.com/5/journal/bootstrap.min.css">
+</head>
+<body>
+<center><h1>qlq e loco ya terminaste</h1>
+<h1>Qlq loco, vote por el pacto historico</h1></center>
+<br>
+    <img src="https://i.redd.it/5uyf63b5yam81.png" class="center" width=26%>
+    <img src="https://i.redd.it/5uyf63b5yam81.png" class="center" width=44%>
+    <img src="https://i.redd.it/5uyf63b5yam81.png" class="center" width=26%>
+</body>
+
+</html>
